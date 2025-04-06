f409de81f742598954f5d47a9de83694b680933e
Karan Kumar  Sahu <102219640+Karan09823@users.noreply.github.com>
2025-04-06 17:49:15 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Shield, User, Stethoscope, Eye, EyeOff } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-
+const API_URL="https://hospital-management-internsahala.vercel.app"
 const Login = () => {
   const navigate = useNavigate();
   const [selectedRole, setSelectedRole] = useState('patient');
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch(`${API_URL}/api/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
